bc6634f8669e2791c02bb04932dcd33989fc1984
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-07-16 16:45:47 +0200

    Add packages, fix header includes.

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,28 @@
 add_rules("mode.debug", "mode.release")
 
-set_warnings("everything", "error", "extra", "pedantic")
+set_warnings("error", "everything", "extra", "pedantic")
 add_cxxflags("-Wconversion", "-Wshadow")
 -- -Wdouble-promotion
 
-set_languages("c11")
+set_languages("c++20")
 set_version("0.1.0")
 -- set_optimize("fastest")
 
+add_requires("taocpp-json")
+add_requires("snitch")
+
+
 target("node_crunch2")
     set_kind("shared")
-    add_files("src/*.cpp", "src/*.hpp")
+    add_files("src/*.cpp")
+    add_packages("taocpp-json")
+    -- add_headerfiles("src/*.hpp")
+    -- add_includedirs("src")
 
 target("test")
     set_kind("binary")
-    add_files("test/*.cpp", "test/*.hpp")
+    add_files("test/*.cpp")
+    add_packages("snitch")
     add_deps("node_crunch2")
+    add_includedirs("src")
+    -- add_headerfiles("test/*.hpp")

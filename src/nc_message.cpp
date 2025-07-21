b36d50cdbedf3d2446763e657a9ebae9f6d652d2
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-07-21 16:15:14 +0200

    Add first code for message.

--- src/nc_message.cpp
+++ src/nc_message.cpp
@@ -6,5 +6,30 @@
     This file defines the message type and decoding / encoding
 */
 
+// STD includes:
+#include <cstdint>
+#include <vector>
+#include <string>
+
+// Local includes:
 #include "nc_message.hpp"
 
+std::vector<uint8_t> encode_message(std::vector<uint8_t> message, std::string secret_key) {
+    std::vector<uint8_t> result;
+
+    // 1. Compress message:
+
+    // 2. Encrypt message:
+
+    return result;
+}
+
+std::vector<uint8_t> decode_message(std::vector<uint8_t> message, std::string secret_key) {
+    std::vector<uint8_t> result;
+
+    // 1. Decrypt message:
+
+    // 2. Decompress message:
+
+    return result;
+}
--- src/nc_message.hpp
+++ src/nc_message.hpp
@@ -5,3 +5,32 @@
 
     This file defines the message type and decoding / encoding
 */
+
+#ifndef FILE_NC_MESSAGE_HPP_INCLUDED
+#define FILE_NC_MESSAGE_HPP_INCLUDED
+
+// STD includes:
+#include <cstdint>
+#include <vector>
+#include <string>
+
+enum struct NCMessageType : uint8_t {
+    Heartbeat = 0,
+    HeartbeatOK = 1,
+    HeartbeatError = 2,
+    Init = 3,
+    InitOK = 4,
+    InitError = 5,
+    NewDataFromServer = 6,
+    NewResultFromNode = 7,
+    NodeNeedsMoreData = 8,
+    ResultOK = 9,
+    ConnectionError = 10,
+    Quit = 11
+};
+
+std::vector<uint8_t> encode_message(std::vector<uint8_t> message, std::string secret_key);
+
+std::vector<uint8_t> decode_message(std::vector<uint8_t> message, std::string secret_key);
+
+#endif // FILE_NC_MESSAGE_HPP_INCLUDED

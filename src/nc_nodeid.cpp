7da8796ea21b497d0d2b4a84b3d62790b52f3b24
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-07-21 16:14:58 +0200

    No longer needed. Implementation in small header file.

--- src/nc_nodeid.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    Node Crunch2
-    Written by Willi Kappler, MIT License
-    https://github.com/willi-kappler/node_crunch2
-
-    This file defines the node id
-*/
-
-#include "nc_nodeid.hpp"

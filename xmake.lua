e283609745645c8927a31ec0550d0024ec4124f8
Willi Kappler <willi.kappler.gm@gmail.com>
2025-07-22 22:49:17 +0200

    Add compression package (lz4).

--- xmake.lua
+++ xmake.lua
@@ -10,12 +10,14 @@ set_version("0.1.0")
 
 add_requires("taocpp-json")
 add_requires("snitch")
+add_requires("lz4")
 
 
 target("node_crunch2")
     set_kind("shared")
     add_files("src/*.cpp")
     add_packages("taocpp-json")
+    add_packages("lz4")
 
 target("nc_test")
     set_kind("binary")

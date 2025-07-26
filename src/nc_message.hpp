b3b96bd50ee3cfbd45f11f98eba97a8b646ac22e
Willi Kappler <willi.kappler.gm@gmail.com>
2025-07-26 23:15:14 +0200

    Add nodiscard to function signature.

--- src/nc_message.hpp
+++ src/nc_message.hpp
@@ -19,8 +19,8 @@
 #include "nc_message_errors.hpp"
 #include "nc_message_types.hpp"
 
-std::expected<NCEncodedMessage, NCMessageError> nc_encode_message(NCRawMessage const& message, std::string const& secret_key);
+[[nodiscard]] std::expected<NCEncodedMessage, NCMessageError> nc_encode_message(NCRawMessage const& message, std::string const& secret_key);
 
-std::expected<NCDecodedMessage, NCMessageError> nc_decode_message(NCEncodedMessage const& message, std::string const& secret_key);
+[[nodiscard]] std::expected<NCDecodedMessage, NCMessageError> nc_decode_message(NCEncodedMessage const& message, std::string const& secret_key);
 
 #endif // FILE_NC_MESSAGE_HPP_INCLUDED

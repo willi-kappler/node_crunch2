1c40df087ff625f81c1a0e44e1942d684300d856
Willi Kappler <willi.kappler.gm@gmail.com>
2025-07-26 23:15:28 +0200

    Add nodiscard to function signature.

--- src/nc_encryption.hpp
+++ src/nc_encryption.hpp
@@ -19,8 +19,8 @@
 #include "nc_message_errors.hpp"
 #include "nc_message_types.hpp"
 
-std::expected<NCEncodedMessage, NCMessageError> nc_encrypt_message(NCCompressedMessage const& message, std::string const& secret_key);
+[[nodiscard]] std::expected<NCEncodedMessage, NCMessageError> nc_encrypt_message(NCCompressedMessage const& message, std::string const& secret_key);
 
-std::expected<NCCompressedMessage, NCMessageError> nc_decrypt_message(NCEncodedMessage const& message, std::string const &secret_key);
+[[nodiscard]] std::expected<NCCompressedMessage, NCMessageError> nc_decrypt_message(NCEncodedMessage const& message, std::string const &secret_key);
 
 #endif // FILE_NC_ENCRYPTION_HPP_INCLUDED

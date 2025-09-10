f0587e22da3bf24bf2b55121dbf01dd9b9e7d4bc
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-09-10 17:36:48 +0200

    Finish network classes.

--- src/nc_network.cpp
+++ src/nc_network.cpp
@@ -36,4 +36,53 @@ void nc_send_data(std::vector<uint8_t> const data, tcp::socket& socket) {
 
     return result;
 }
+
+void NCNetworkSocket::nc_send_data(std::vector<uint8_t> const data) {
+    uint32_t data_size = static_cast<uint32_t>(data.size());
+    std::array<uint8_t, 4> size_bytes;
+    nc_to_big_endian_bytes(data_size, size_bytes);
+
+    asio::write(socket_intern, asio::buffer(size_bytes));
+    asio::write(socket_intern, asio::buffer(data));
+}
+
+[[nodiscard]] std::vector<uint8_t> NCNetworkSocket::nc_receive_data() {
+    std::array<uint8_t, 4> size_bytes;
+
+    asio::read(socket_intern, asio::buffer(size_bytes));
+    uint32_t data_size = nc_from_big_endian_bytes(size_bytes);
+
+    std::vector<uint8_t> result(data_size);
+    if (data_size > 0) {
+        asio::read(socket_intern, asio::buffer(result));
+    }
+
+    return result;
+}
+
+NCNetworkSocket::NCNetworkSocket(tcp::socket &socket): socket_intern(std::move(socket)) {}
+
+NCNetworkSocket NCNetworkClient::nc_connect() {
+    tcp::socket socket(io_context_intern);
+    // tcp::endpoint connection = asio::connect(socket, endpoints_intern);
+    asio::connect(socket, endpoints_intern);
+    return NCNetworkSocket(socket);
+}
+
+NCNetworkClient::NCNetworkClient(std::string_view server, uint16_t port):
+    io_context_intern(),
+    resolver_intern(io_context_intern),
+    endpoints_intern(resolver_intern.resolve(server, std::to_string(port)))
+    {}
+
+NCNetworkSocket NCNetworkServer::nc_accept() {
+    tcp::socket socket(io_context_intern);
+    acceptor_intern.accept(socket);
+    return NCNetworkSocket(socket);
+}
+
+NCNetworkServer::NCNetworkServer(uint16_t server_port):
+    io_context_intern(),
+    acceptor_intern(io_context_intern, tcp::endpoint(tcp::v4(), server_port))
+    {}
 }
--- src/nc_network.hpp
+++ src/nc_network.hpp
@@ -16,15 +16,83 @@
 // External includes:
 #include <asio.hpp>
 
-// Local includes:
-//#include "nc_util.hpp"
-
 using asio::ip::tcp;
 
 namespace NodeCrunch2 {
 void nc_send_data(std::vector<uint8_t> const data, tcp::socket& socket);
-
 [[nodiscard]] std::vector<uint8_t> nc_receive_data(tcp::socket& socket);
+
+class NCNetworkSocket {
+    public:
+        virtual void nc_send_data(std::vector<uint8_t> const data);
+        [[nodiscard]] virtual std::vector<uint8_t> nc_receive_data();
+
+        // Constructor:
+        NCNetworkSocket(tcp::socket &socket);
+
+        // Destructor:
+        virtual ~NCNetworkSocket() = default;
+
+        // Default special member functions:
+        NCNetworkSocket(NCNetworkSocket&&) = default;
+        NCNetworkSocket(const NCNetworkSocket&) = default;
+
+        // Disable all other special member functions:
+        NCNetworkSocket& operator=(const NCNetworkSocket&) = delete;
+        NCNetworkSocket& operator=(NCNetworkSocket&&) = delete;
+
+    private:
+        tcp::socket socket_intern;
+};
+
+class NCNetworkClient {
+    public:
+        virtual NCNetworkSocket nc_connect();
+        //virtual NCNetworkSocket nc_connect_to(std::string_view server, std::string_view port);
+
+        // Constructor:
+        NCNetworkClient(std::string_view server, uint16_t port);
+
+        // Destructor:
+        virtual ~NCNetworkClient() = default;
+
+        // Default special member functions:
+        NCNetworkClient(NCNetworkClient&&) = default;
+        NCNetworkClient(const NCNetworkClient&) = default;
+
+        // Disable all other special member functions:
+        NCNetworkClient& operator=(const NCNetworkClient&) = delete;
+        NCNetworkClient& operator=(NCNetworkClient&&) = delete;
+
+    private:
+        asio::io_context io_context_intern;
+        tcp::resolver resolver_intern;
+        tcp::resolver::results_type endpoints_intern;
+};
+
+class NCNetworkServer {
+    public:
+        virtual NCNetworkSocket nc_accept();
+
+        // Constructor:
+        NCNetworkServer(uint16_t server_port);
+
+        // Destructor:
+        virtual ~NCNetworkServer() = default;
+
+        // Default special member functions:
+        NCNetworkServer(NCNetworkServer&&) = default;
+        NCNetworkServer(const NCNetworkServer&) = default;
+
+        // Disable all other special member functions:
+        NCNetworkServer& operator=(const NCNetworkServer&) = delete;
+        NCNetworkServer& operator=(NCNetworkServer&&) = delete;
+
+    private:
+        asio::io_context io_context_intern;
+        tcp::acceptor acceptor_intern;
+};
+
 }
 
 #endif // FILE_NC_NETWORK_HPP_INCLUDED

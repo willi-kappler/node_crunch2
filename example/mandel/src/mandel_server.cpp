f0d1bb649ccda508917178095dde7f1a4c7a39c3
Willi Kappler <willi.kappler.gm@gmail.com>
2026-01-22 00:17:01 +0100

    Add more code to server.

--- example/mandel/src/mandel_server.cpp
+++ example/mandel/src/mandel_server.cpp
@@ -43,14 +43,51 @@ void MandelServerProcessor::nc_save_data() {
 
 void MandelServerProcessor::nc_node_timeout(NCNodeID node_id) {
     spdlog::debug("Node timeout: {}", node_id);
+
+    if (node_map.contains(node_id)) {
+        uint32_t i = node_map[node_id];
+        if (mandel_job[i] == JobStatus::Processing) {
+            mandel_job[i] = JobStatus::UnProcessed;
+        } else {
+            spdlog::debug("Job at {} not processing.", i);
+        }
+        node_map.erase(node_id);
+    } else {
+        spdlog::debug("Timeout node {} not found in map.", node_id);
+    }
 }
 
 [[nodiscard]] std::vector<uint8_t> MandelServerProcessor::nc_get_new_data(NCNodeID node_id) {
     spdlog::debug("New data for node: {}", node_id);
+    std::vector<uint8_t> result;
 
+    for (uint32_t i = 0; i < mandel_data_intern.height; i++) {
+        if (mandel_job[i] == JobStatus::UnProcessed) {
+            mandel_job[i] = JobStatus::Processing;
+            node_map[node_id] = i;
+            // TODO: convert i to result
+            // result = i;
+            return result;
+        }
+    }
+
+    return result;
 }
 
 void MandelServerProcessor::nc_process_result(NCNodeID node_id, std::vector<uint8_t> result) {
     spdlog::debug("Processed data from node: {}", node_id);
 
+    if (node_map.contains(node_id)) {
+        uint32_t i = node_map[node_id];
+
+        if (mandel_job[i] == JobStatus::Processing) {
+            mandel_job[i] = JobStatus::Done;
+            // TODO: store data
+            // mandel_image[i] = result;
+        } else {
+            spdlog::error("Not processing at index {}", i);
+        }
+    } else {
+        spdlog::error("Node not found in node map: {}", node_id);
+    }
 }
